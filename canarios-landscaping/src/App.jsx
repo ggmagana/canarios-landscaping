125838c76de6c9c3b71dd8055617c208c85511fc
ggmagana <ggmwebsites@outlook.com>
2026-03-22 15:52:25 -0400

    Testing

--- canarios-landscaping/src/App.jsx
+++ canarios-landscaping/src/App.jsx
@@ -1,28 +1,33 @@
-import { useTranslation } from "react-i18next";
-import { Routes, Route } from "react-router-dom";
-import LanguageToggle from "./components/LanguageToggle";
+// import { useTranslation } from "react-i18next";
+// import { Routes, Route } from "react-router-dom";
+// import LanguageToggle from "./components/LanguageToggle";
 
 
-import Home from "./pages/Home";
-import Quote from "./pages/Quote";
-import ServiceDetail from "./pages/ServiceDetail";
+// import Home from "./pages/Home";
+// import Quote from "./pages/Quote";
+// import ServiceDetail from "./pages/ServiceDetail";
 
+// function App() {
+//   const { i18n } = useTranslation();
+
+//   return (
+//     <>
+//       {/* Language Toggle */}
+//       <LanguageToggle />
+
+//       {/* Routes */}
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/quote" element={<Quote />} />
+//         <Route path="/service/:name" element={<ServiceDetail />} />
+//       </Routes>
+//     </>
+//   );
+// }
+
+// export default App;
 function App() {
-  const { i18n } = useTranslation();
-
-  return (
-    <>
-      {/* Language Toggle */}
-      <LanguageToggle />
-
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quote" element={<Quote />} />
-        <Route path="/service/:name" element={<ServiceDetail />} />
-      </Routes>
-    </>
-  );
+  return <h1 style={{color: "green"}}>APP WORKING ✅</h1>;
 }
 
 export default App;
